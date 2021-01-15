d0bbff4adf280bded102df43213abde678db8711
chenguangsmy <chenguangsmy@gmail.com>
2021-01-15 17:49:23 -0500

    decrease KxBx, un-uniform perturb, iteration_Max=1

--- WAM_OffLineTesting/CustomController.cpp
+++ WAM_OffLineTesting/CustomController.cpp
@@ -68,25 +68,27 @@ public:
 	 	B_q = 0.1*K_q;
 
 	 	// Nominal joint space postion
-		input_q_0[0] = -1.570;
-		input_q_0[1] =  0.008; 
-		input_q_0[2] =  0.008; 
+		input_q_0[0] = -1.571;
+		input_q_0[1] =  0.007; 
+		input_q_0[2] = -0.004; 
 		input_q_0[3] =  1.570; 
 
 		//End-effector stiffness
-		K_x(0,0) = 2000.0;
-		K_x(1,1) = 2000.0;
+		K_x(0,0) = 1000.0;
+		K_x(1,1) = 1000.0;
 	 	K_x(2,2) = 0.0; 
 
 		//End-effector damping
-	 	B_x = 0.02*K_x;
+	 	B_x(0,0) = 20;
+    B_x(1,1) = 20;
+    B_x(1,1) = 20;
 
 		// Nominal end-effector potion	(NEED TO CHECK THIS BEFORE TESTING)
-		input_x_0[0] = -0.409;  //position: raise hand on desk
+		input_x_0[0] = -0.513;  //position: raise hand on desk
 		input_x_0[1] = 0.482;
-		input_x_0[2] = -0.04;
+		input_x_0[2] = -0.002;
 
-		iteration_MAX = 4;
+		iteration_MAX = 1;
 		iteration = 0; // do not input and output here
 		prevPret[0] = 0.0;
 		prevPret[1] = 0.0;
@@ -228,7 +230,7 @@ protected:
 			f_pretOutput[2] = 0.0;
 
 			// Make Preturbation unifore amplitude
-			pretAmplitude = 2.0;
+/*			pretAmplitude = 2.0;
 			if (f_pretOutput[0] >= 0 ) {
 				f_pretOutput[0] = pretAmplitude;
 			} else if (f_pretOutput[0] < 0 ){
@@ -240,6 +242,7 @@ protected:
 			} else if (f_pretOutput[1] < 0 ){
 				f_pretOutput[1] = -pretAmplitude;
 			}
+*/
 		
 			f_pret[0] = f_pretOutput[0];
 			f_pret[1] = f_pretOutput[1];
