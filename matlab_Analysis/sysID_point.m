98546964c1e2d68a4f0ddc3c79061e4b0b22635f
jameshermus <jhermus@mit.edu>
2021-03-25 08:56:12 -0800

    push to change branches

--- matlab_Analysis/sysID_point.m
+++ matlab_Analysis/sysID_point.m
@@ -22,8 +22,8 @@ pathh = '/Users/jhermus/Documents/School/MIT/Research/Schwartz_Collaboration/pro
 
 %           filee = '20210203aft04.csv'; % | K_x = 0, B_x = 0, it_Max = 8, pretAmplitude = 0.0 |
 %             filee1 = '20210203aft05.csv'; % | K_x = 1000, B_x = 20, it_Max = 8, pretAmplitude = 0.0 |
-            filee1 = '20210203aft06.csv'; % | K_x = 1000, B_x = 20, it_Max = 8, pretAmplitude =10.0 |
-            filee2 = '20210203aft07.csv'; % | K_x = 500,  B_x = 20, it_Max = 8, pretAmplitude =10.0 |
+%             filee1 = '20210203aft06.csv'; % | K_x = 1000, B_x = 20, it_Max = 8, pretAmplitude =10.0 |
+%             filee2 = '20210203aft07.csv'; % | K_x = 500,  B_x = 20, it_Max = 8, pretAmplitude =10.0 |
 %           filee1 = '20210203aft08.csv'; % | K_x = 1000, B_x = 20, it_Max =15, pretAmplitude =10.0 |
 %           filee4 = '20210203aft09.csv'; % | K_x = 500,  B_x = 20, it_Max =15, pretAmplitude =10.0 |
 %           filee1 = '20210203aft10.csv'; % | K_x = 500,  B_x = 20, it_Max =15, pretAmplitudex =16.0, pretAmplitudex =8.0 |
@@ -33,13 +33,56 @@ pathh = '/Users/jhermus/Documents/School/MIT/Research/Schwartz_Collaboration/pro
 %           filee2 = '20210204aft01.csv'; % Kx = 500, Bx = 0, maxiter = 1, pret amplitude = 2
 
 %           filee3 = '20210204aft02.csv'; % K_x=1000, B_x=20, itermax = 1, pretAmplitude = 10, not removed FT cable in 02 and 03
-            filee3 = '20210204aft03.csv'; % K_x=500,   B_x=0, itermax = 8, pretAmplitude = 10, really big shake in y, up to 10cm
+%             filee3 = '20210204aft03.csv'; % K_x=500,   B_x=0, itermax = 8, pretAmplitude = 10, really big shake in y, up to 10cm
 %           filee1 = '20210204aft04.csv'; % K_x=2500, B_x=20, itermax = 8, pretAmplitude = 6
 %           filee2 = '20210204aft05.csv'; % K_x=2500, B_x=20, itermax = 8, pretAmplitude = 6, with arm holded |
+            
+        % PretAmplitude = 6, iteration = 8
+%         filee1 = '20210218aft05.csv'; % Kx=2500, Bx=10
+%         filee2 = '20210218aft06.csv'; % Kx=2500, Bx=0
+%         filee3 = '20210218aft07.csv'; % Kx=500, Bx=10
+%         filee4 = '20210218aft08.csv'; % Kx=500, Bx=0
+%         filee5 = '20210218aft09.csv'; % Kx=2500, Bx=[-10,10]
+        
+        % Spring Testing
+        filee1 = '20210311aft00.csv';
+        filee2 = '20210311aft01.csv';
+        filee3 = '20210311aft02.csv';
+        filee4 = '20210311aft03.csv';
+        filee5 = '20210311aft04.csv';
+        
+        filee6 = '20210311aft05.csv';
+        filee7 = '20210311aft06.csv';
+        filee8 = '20210311aft07.csv';
+        filee9 = '20210311aft08.csv';
+        filee10 = '20210311aft09.csv';
+
+%         filee1 = '20210311aft01.csv';
+%         filee2 = '20210311aft06.csv';
 
 
-fileNamess = {[pathh,filee1],[pathh,filee2],[pathh,filee3]};
-namesString =  {'K:1000, B:20','K:500, B:20','K:500, B:0'};
+% fileNamess = {[pathh,filee1],[pathh,filee2]};
+% namesString =  {'robot','robot+spring'};
+
+
+% fileNamess = {[pathh,filee1],[pathh,filee2],[pathh,filee3],[pathh,filee4],[pathh,filee5]};
+% namesString =  {'K:2500, B:10','K:2500, B:0','K:500, B:20','K:500, B:0','Kx=2500, Bx=[-10,10]'};
+% namesString =  {'1','2','3','4','5'};
+
+fileNamess = {[pathh,filee1],[pathh,filee2],[pathh,filee3],[pathh,filee4],[pathh,filee5],...
+              [pathh,filee6],[pathh,filee7],[pathh,filee8],[pathh,filee9],[pathh,filee10]};
+namesString =  {'1','2','3','4','5','6','7','8','9','10'};
+
+
+
+% fileNamess = {[pathh,filee1]};
+% namesString =  {'Robot'};
+
+% filee1 = '20210203aft06.csv'; % | K_x = 1000, B_x = 20, it_Max = 8, pretAmplitude = 10.0 |
+% filee2 = '20210203aft07.csv'; % | K_x = 500,  B_x = 20, it_Max = 8, pretAmplitude = 10.0 |
+% filee3 = '20210204aft03.csv'; % | K_x = 500,  B_x =  0, it_Max = 8, pretAmplitude = 10.0, really big shake in y, up to 10cm
+% fileNamess = {[pathh,filee1],[pathh,filee2],[pathh,filee3]};
+% namesString =  {'K:1000, B:20','K:500, B:20','K:500, B:0'};
 
 % namesString =  {'Robot','Robot+Hand'};
 
@@ -62,7 +105,7 @@ function [A] = leeID(fileNamee)
 % Impedance Identification in principal axis directions (DP and IE)
 
 %% Variables
-nfft=12000;
+nfft=5000;
 nFreq = nfft/2+1;
 Hz = 500;
 runningTime = 5*55;
@@ -118,8 +161,8 @@ PC21_s = zeros(nFreq,1);
 
 data = importdata(fileNamee);
 
-dexStart = 2e+4;
-dexEnd = size(data,1)-1e+4;
+dexStart = 20*Hz; %2e+4;
+dexEnd = size(data,1)-20*Hz;
 dexRange = [dexStart:dexEnd];
 t = data(dexRange,1);
 X_m = data(dexRange,10);
@@ -144,10 +187,10 @@ Y1=X_m;
 Y2=Y_m;
 
 figure; 
-subplot(2,2,1); plot(X1);
-subplot(2,2,2); plot(X2);
-subplot(2,2,3); plot(Y1);
-subplot(2,2,4); plot(Y2);
+subplot(2,2,1); plot(t,X1);
+subplot(2,2,2); plot(t,X2);
+subplot(2,2,3); plot(t,Y1);
+subplot(2,2,4); plot(t,Y2);
 
 % figure; plot(t(1:500)-t(1), X1(1:500),'linewidth',2.5);
 % xlabel('Time (s)'); ylabel('Force (N)'); ylim([-8 8]); 
@@ -304,37 +347,119 @@ yUpperLim = 180.0;
 
 X = figure;
 XX = figure;
-colorr = {'-b','-r','-k','--b','--r','--k'};
+XXX = figure;
+% colorr = {'-b','-r','-k','--b','--r','--k'};
 % colorr = {'-b','-r','-k'};
+colorr = {'-b','-b','-b','-b','-b','-r','-r','-r','-r','-r'};
+
+
+FS = 12;
+
+% figure(X);
+% for i = 1:length(A)
+%     
+%     % Magnitude plot of ankle impedance
+%     ax1 = subplot(2,2,1);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     loglog(A(i).TF_freq,A(i).Z11_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
+%     xlabel('frequency(Hz)','fontWeight','bold','fontSize',FS);
+%     ylabel('magnitude (abs)','fontWeight','bold','fontSize',FS);
+%     title('Z11','fontWeight','bold','fontSize',FS);
+%     
+%     ax2 = subplot(2,2,2);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     loglog(A(i).TF_freq,A(i).Z12_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
+%     xlabel('frequency (Hz)','fontWeight','bold','fontSize',FS);
+%     ylabel('magnitude (abs)','fontWeight','bold','fontSize',FS);
+%     title('Z12','fontWeight','bold','fontSize',FS);
+%     
+%     ax3 = subplot(2,2,3);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     loglog(A(i).TF_freq,A(i).Z21_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
+%     xlabel('frequency (Hz)','fontWeight','bold','fontSize',FS);
+%     ylabel('magnitude (abs)','fontWeight','bold','fontSize',FS);
+%     title('Z21','fontWeight','bold','fontSize',FS);
+%     
+%     ax4 = subplot(2,2,4);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     loglog(A(i).TF_freq,A(i).Z22_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
+%     xlabel('frequency (Hz)','fontWeight','bold','fontSize',FS);
+%     ylabel('magnitude (abs)','fontWeight','bold','fontSize',FS);
+%     title('Z22','fontWeight','bold','fontSize',FS);
+%     
+%     linkaxes([ax1,ax2,ax3,ax4],'x');
+%     
+% end
+% legend(namesString);
+% 
+% figure(XX);
+% for i = 1:length(A)
+%     
+%     ax1 = subplot(2,2,1);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     semilogx(A(i).TF_freq,A(i).Z11_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     axis([xLowerLim xUpperLim 0 180]);
+%     xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
+%     title('Z11','fontWeight','bold','fontSize',FS);
+%     
+%     ax2 = subplot(2,2,2);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     semilogx(A(i).TF_freq,A(i).Z12_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     axis([xLowerLim xUpperLim 0 180]);
+%     xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
+%     title('Z12','fontWeight','bold','fontSize',FS);
+%     
+%     ax3 = subplot(2,2,3);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     semilogx(A(i).TF_freq,A(i).Z21_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     axis([xLowerLim xUpperLim 0 180]);
+%     xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
+%     title('Z21','fontWeight','bold','fontSize',FS);
+% 
+%     ax4 = subplot(2,2,4);
+%     set(gca,'fontWeight','bold','fontSize',FS);
+%     semilogx(A(i).TF_freq,A(i).Z22_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+%     axis([xLowerLim xUpperLim 0 180]);
+%     xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
+%     title('Z22','fontWeight','bold','fontSize',FS);
+% 
+%     linkaxes([ax1,ax2,ax3,ax4],'x');
+%     
+% end
+% legend(namesString);
 
 figure(X);
 for i = 1:length(A)
     
     % Magnitude plot of ankle impedance
     ax1 = subplot(2,2,1);
-    set(gca,'fontWeight','bold','fontSize',12);
-    loglog(A(i).TF_freq,A(i).Z11_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+    set(gca,'fontWeight','bold','fontSize',FS);
+    p1(i) = loglog(A(i).TF_freq,A(i).Z11_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
     %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
-    xlabel('frequency(Hz)','fontWeight','bold','fontSize',14);
-    ylabel('magnitude (abs)','fontWeight','bold','fontSize',14);
-    title('Z11','fontWeight','bold','fontSize',16);
+    xlabel('frequency(Hz)','fontWeight','bold','fontSize',FS);
+    ylabel('magnitude (abs)','fontWeight','bold','fontSize',FS);
+    title('Z11','fontWeight','bold','fontSize',FS);
     
     ax2 = subplot(2,2,2);
-    set(gca,'fontWeight','bold','fontSize',12);
+    set(gca,'fontWeight','bold','fontSize',FS);
     loglog(A(i).TF_freq,A(i).Z22_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
     %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
-    xlabel('frequency (Hz)','fontWeight','bold','fontSize',14);
-    ylabel('magnitude (abs)','fontWeight','bold','fontSize',14);
-    title('Z22','fontWeight','bold','fontSize',16);
+    xlabel('frequency (Hz)','fontWeight','bold','fontSize',FS);
+    ylabel('magnitude (abs)','fontWeight','bold','fontSize',FS);
+    title('Z22','fontWeight','bold','fontSize',FS);
     
     ax3 = subplot(2,2,3);
-    set(gca,'fontWeight','bold','fontSize',12);
+    set(gca,'fontWeight','bold','fontSize',FS);
     semilogx(A(i).TF_freq,A(i).Z11_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
     axis([xLowerLim xUpperLim 0 180]);
     xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
     
     ax4 = subplot(2,2,4);
-    set(gca,'fontWeight','bold','fontSize',12);
+    set(gca,'fontWeight','bold','fontSize',FS);
     semilogx(A(i).TF_freq,A(i).Z22_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
     axis([xLowerLim xUpperLim 0 180]);
     xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
@@ -342,7 +467,7 @@ for i = 1:length(A)
     linkaxes([ax1,ax2,ax3,ax4],'x');
     
 end
-legend(namesString);
+legend([p1(1) p1(6)],{'Robot','Robot+Spring'});
 
 %% Sanity check Expected Dynamics
 % opts = bodeoptions('cstprefs');
@@ -386,11 +511,11 @@ disp('test');
 
 %% Partial Coherence Plot
 
-figure(XX);
+figure(XXX);
 for i = 1:length(A)
-    ax1 = subplot(2,2,1,'XScale','log');
+    ax1 = subplot(2,2,1);
     set(gca,'fontWeight','bold','fontSize',12);
-    plot(A(i).TF_freq,A(i).PC11(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+    semilogx(A(i).TF_freq,A(i).PC11(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
     axis([xLowerLim xUpperLim 0 1]);
     xlabel('frequence (Hz)','fontWeight','bold','fontSize',14);
     ylabel('\gamma^2','fontWeight','bold','fontSize',14);
@@ -422,62 +547,97 @@ for i = 1:length(A)
     
     linkaxes([ax1,ax2,ax3,ax4],'x');
 end
-legend(namesString);
+% legend(namesString);
 
 
-%% Plot diffrence
+% %% Plot diffrence
+% figure;
+% % Magnitude plot of ankle impedance
+% ax1 = subplot(2,2,1);
+% set(gca,'fontWeight','bold','fontSize',12);
+% loglog(A(1).TF_freq,A(2).Z11_mag(:,1)-A(1).Z11_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+% %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
+% xlabel('frequency(Hz)','fontWeight','bold','fontSize',14);
+% ylabel('magnitude (abs)','fontWeight','bold','fontSize',14);
+% title('Z11','fontWeight','bold','fontSize',16);
+% 
+% ax2 = subplot(2,2,2);
+% set(gca,'fontWeight','bold','fontSize',12);
+% loglog(A(i).TF_freq,A(2).Z22_mag(:,1)-A(1).Z22_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+% %axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
+% xlabel('frequency (Hz)','fontWeight','bold','fontSize',14);
+% ylabel('magnitude (abs)','fontWeight','bold','fontSize',14);
+% title('Z22','fontWeight','bold','fontSize',16);
+% 
+% ax3 = subplot(2,2,3);
+% set(gca,'fontWeight','bold','fontSize',12);
+% semilogx(A(1).TF_freq,A(2).Z11_phi(:,1)-A(1).Z11_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+% axis([xLowerLim xUpperLim 0 180]);
+% xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
+% 
+% ax4 = subplot(2,2,4);
+% set(gca,'fontWeight','bold','fontSize',12);
+% semilogx(A(1).TF_freq,A(2).Z22_phi(:,1)-A(1).Z22_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
+% axis([xLowerLim xUpperLim 0 180]);
+% xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
+% 
+% linkaxes([ax1,ax2,ax3,ax4],'x');
+
+% figure; 
+
+dexStiff = find( 0.2 < A(i).TF_freq & A(i).TF_freq < 1.3 );
+ K_r = (A(1).Z22_mag(dexStiff,1) +...
+           A(2).Z22_mag(dexStiff,1) +...
+           A(3).Z22_mag(dexStiff,1) +...
+           A(4).Z22_mag(dexStiff,1) +...
+           A(5).Z22_mag(dexStiff,1))./5;
+K_rh = (A(6).Z22_mag(dexStiff,1) +...
+          A(7).Z22_mag(dexStiff,1) +...
+          A(8).Z22_mag(dexStiff,1) +...
+          A(9).Z22_mag(dexStiff,1) +...
+          A(10).Z22_mag(dexStiff,1))./5;
+      
+K_hat = K_rh-K_r;
+
+            
+% figure;plot(A(1).TF_freq(dexStiff),K_r);
+% figure;plot(A(1).TF_freq(dexStiff),K_rh);
+% figure;plot(A(1).TF_freq(dexStiff),K_hat);
+      
+
 figure;
-% Magnitude plot of ankle impedance
-ax1 = subplot(2,2,1);
-set(gca,'fontWeight','bold','fontSize',12);
-loglog(A(1).TF_freq,A(2).Z11_mag(:,1)-A(1).Z11_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
-%axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
-xlabel('frequency(Hz)','fontWeight','bold','fontSize',14);
-ylabel('magnitude (abs)','fontWeight','bold','fontSize',14);
-title('Z11','fontWeight','bold','fontSize',16);
+ax1 = errorbar(1.4,mean(K_hat),std(K_hat),'linewidth',2,'color','b'); hold on;
+ax2 = errorbar(1.45,315.2,8.8*2,'linewidth',2,'color','r');
+plot(A(i).TF_freq(dexStiff), K_hat,'ob','linewidth',2);
+plot([0.2,1.5],[mean(K_hat),mean(K_hat)],'-b','linewidth',2);
+plot([0.2,1.5],[315.2,315.2],'-r','linewidth',2);
 
-ax2 = subplot(2,2,2);
-set(gca,'fontWeight','bold','fontSize',12);
-loglog(A(i).TF_freq,A(2).Z22_mag(:,1)-A(1).Z22_mag(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
-%axis([xLowerLim xUpperLim yLowerLim yUpperLim]);
-xlabel('frequency (Hz)','fontWeight','bold','fontSize',14);
-ylabel('magnitude (abs)','fontWeight','bold','fontSize',14);
-title('Z22','fontWeight','bold','fontSize',16);
+xlabel('Frequency (Hz)');
+ylabel('Stiffness (N/m)');
+set(gca,'fontsize',18); ylim([0 400]); xlim([0.2 1.5]);
+legend('Stiffness Estimate','Manufacturer Stiffness \pm tolerance','location','south');
 
-ax3 = subplot(2,2,3);
-set(gca,'fontWeight','bold','fontSize',12);
-semilogx(A(1).TF_freq,A(2).Z11_phi(:,1)-A(1).Z11_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
-axis([xLowerLim xUpperLim 0 180]);
-xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
-
-ax4 = subplot(2,2,4);
-set(gca,'fontWeight','bold','fontSize',12);
-semilogx(A(1).TF_freq,A(2).Z22_phi(:,1)-A(1).Z22_phi(:,1),colorr{i},'LineWidth',2); hold on; grid on; box on;
-axis([xLowerLim xUpperLim 0 180]);
-xlabel('frequency (Hz)','fontWeight','bold','fontSize',14); ylabel('phase (deg)','fontWeight','bold','fontSize',14);
-
-linkaxes([ax1,ax2,ax3,ax4],'x');
-
-% axes(ax2)
-figure; 
-% Sanity check
-opts = bodeoptions('cstprefs');
-opts.MagUnits = 'abs';
-opts.MagScale = 'log';
-s = tf('s');
-
-m = 2.5;
-b = 7;
-k = 31;
-[MAG,PHASE,W] = bode(m*s^2+b*s+k);
-MAG = squeeze(MAG);
-PHASE = squeeze(PHASE);
-subplot(2,1,1);loglog(W,MAG,'-','linewidth',2.5); grid on; %xlim([W(2) W(end)]); ylim([10^-6 10^-1]);
-ylabel('Amplitude (abs)'); set(gca,'fontsize',18);
-%             yticks([1e-5,1e-3,1e-1]);
-
-subplot(2,1,2); semilogx(W,PHASE,'linewidth',2.5); grid on;
-ylabel('Phase (deg)'); xlabel('Frequency ()'); set(gca,'fontsize',18);
+% 
+% % axes(ax2)
+% figure; 
+% % Sanity check
+% opts = bodeoptions('cstprefs');
+% opts.MagUnits = 'abs';
+% opts.MagScale = 'log';
+% s = tf('s');
+% 
+% m = 2.5;
+% b = 7;
+% k = 31;
+% [MAG,PHASE,W] = bode(m*s^2+b*s+k);
+% MAG = squeeze(MAG);
+% PHASE = squeeze(PHASE);
+% subplot(2,1,1);loglog(W,MAG,'-','linewidth',2.5); grid on; %xlim([W(2) W(end)]); ylim([10^-6 10^-1]);
+% ylabel('Amplitude (abs)'); set(gca,'fontsize',18);
+% %             yticks([1e-5,1e-3,1e-1]);
+% 
+% subplot(2,1,2); semilogx(W,PHASE,'linewidth',2.5); grid on;
+% ylabel('Phase (deg)'); xlabel('Frequency ()'); set(gca,'fontsize',18);
     
 
 end
